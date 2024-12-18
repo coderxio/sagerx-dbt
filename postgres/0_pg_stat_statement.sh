9f8f657752cc6d3b4df99430febea5cd437bfafa
Joey LeGrand <jrlegrand@gmail.com>
2024-12-18 19:52:57 +0000

    Initial cleanup

--- postgres/0_pg_stat_statement.sh
+++
@@ -1,3 +0,0 @@
-sed -i "s/#shared_preload_libraries = ''/shared_preload_libraries = 'pg_stat_statements'\npg_stat_statements.max=10000\npg_stat_statements.track=all/g" /var/lib/postgresql/data/postgresql.conf
-
-echo "Enabled pg_stat_statements"
